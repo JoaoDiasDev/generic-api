0f4ef73cf0350235a0fc31c0bc7dc7e95a55ce52
JoaoDias <joaodiasworking@gmail.com>
2023-01-05 12:01:44 -0300

    Added CORS configurations, still not working sadly, going to remove it for now!

--- joaodias_generic_api/joaodias_generic_api/Program.cs
+++ joaodias_generic_api/joaodias_generic_api/Program.cs
@@ -13,27 +13,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddCors(builder =>
-{
-    builder.AddPolicy("Policy", policy =>
-    {
-        policy.WithOrigins("http://localhost:61960",
-            "https://localhost:61960",
-            "https://joaodiasdev.github.io",
-            "https://joaodiasdev.github.io",
-            "http://localhost",
-            "https://localhost",
-            "https://localhost:5556",
-            "http://localhost:5555",
-            "http://localhost:7251",
-            "https://localhost:7251")
-            .SetIsOriginAllowedToAllowWildcardSubdomains()
-            .AllowAnyHeader()
-            .AllowCredentials()
-            .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
-            .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
-    });
-});
+//builder.Services.AddCors(builder =>
+//{
+//    builder.AddPolicy("Policy", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:61960",
+//            "https://localhost:61960",
+//            "https://joaodiasdev.github.io",
+//            "https://joaodiasdev.github.io",
+//            "http://localhost",
+//            "https://localhost",
+//            "https://localhost:5556",
+//            "http://localhost:5555",
+//            "http://localhost:7251",
+//            "https://localhost:7251")
+//            .SetIsOriginAllowedToAllowWildcardSubdomains()
+//            .AllowAnyHeader()
+//            .AllowCredentials()
+//            .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+//            .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
+//    });
+//});
 
 builder.WebHost.UseUrls("http://localhost:5555", "https://localhost:5556");
 
@@ -62,25 +62,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 
-app.UseCors(builder =>
-{
-    builder.WithOrigins("http://localhost:61960",
-       "https://localhost:61960",
-       "https://joaodiasdev.github.io",
-       "https://joaodiasdev.github.io",
-       "https://localhost:5556",
-       "http://localhost:5555",
-       "http://localhost:7251",
-       "https://localhost:7251")
-       .SetIsOriginAllowedToAllowWildcardSubdomains()
-       .AllowAnyHeader()
-       .AllowCredentials()
-       .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
-       .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
-}
-);
+//app.UseCors(builder =>
+//{
+//    builder.WithOrigins("http://localhost:61960",
+//       "https://localhost:61960",
+//       "https://joaodiasdev.github.io",
+//       "https://joaodiasdev.github.io",
+//       "https://localhost:5556",
+//       "http://localhost:5555",
+//       "http://localhost:7251",
+//       "https://localhost:7251")
+//       .SetIsOriginAllowedToAllowWildcardSubdomains()
+//       .AllowAnyHeader()
+//       .AllowCredentials()
+//       .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+//       .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
+//}
+//);
 
-app.UseCors();
+//app.UseCors();
 
 app.UseAuthentication();
 
